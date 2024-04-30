e4d9ce300e1a6bae41a0728ea6927dafa6f489e6
Usuario <Usuario@CPD03-M05>
2024-04-30 10:47:02 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jmarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nome = { "JOÃO", "PEDRO", "MARIA" };
+
+            Console.WriteLine("Digite um número de 0 a 2: ");
+
+            int i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O nome selecionado foi: " + nome[i]);
+            
+            Console.ReadKey();
+        
+        }
+
+    }
+}
